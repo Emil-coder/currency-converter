baa7d0a303b75960af47d040bd752143784e1a0f
Emil Voltaire <emil.voltaire@54u.se>
2019-11-21 12:42:20 +0100

    Initial commit

---
+++ public/js/store/configureStore.js
@@ -0,0 +1,36 @@
+import { applyMiddleware, createStore } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
+
+var defaultState = {
+  originAmount: '0.00',
+  destinationAmount: '0.00',
+  conversionRate: 1.5
+};
+
+function amount(state = defaultState, action) {
+  if (action.type === 'CHANGE_ORIGIN_AMOUNT') {
+    return {
+      ...state,
+      originAmount: action.data.newAmount
+    }
+  } else if (action.type === 'RECEIVED_CONVERSION_RATE') {
+    return {
+      ...state,
+      conversionRate: action.data.xRate,
+      destinationAmount: action.data.destAmount
+    }
+  }
+  return state
+}
+
+var logger = createLogger({
+  collapsed: true
+});
+
+var store = createStore(
+  amount,
+  applyMiddleware(thunk, logger)
+);
+
+export default store;
